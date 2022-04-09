c7b6d3423cc1b7781df0922fbc2c6b3a0e5213fc
kdescamps <kevindescamps78@gmail.com>
2022-04-09 22:50:21 +0200

    add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { LoggerMiddleware } from './logger/logger.middleware';
 import { PrimeController } from './prime/prime.controller';
 import { PrimeService } from './prime/prime.service';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [HealthModule],
   controllers: [PrimeController],
   providers: [PrimeService],
 })
