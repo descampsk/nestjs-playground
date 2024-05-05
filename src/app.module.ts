64933e681ec82d058e2fb02020ccedbca96f32e9
descampsk <22590202+descampsk@users.noreply.github.com>
2024-05-05 10:34:40 +0000

    feat: add home page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { HealthModule } from './core/health/health.module';
 import { ConditionalModule, ConfigModule } from '@nestjs/config';
 import { LoggerConfig } from './core/logger/logger.config';
 import { PrimeModule } from './modules/prime/prime.module';
 import { Auth0Module } from './modules/auth0/auth0.module';
+import { join } from 'path';
 
 const conditionalModules = [PrimeModule, Auth0Module];
 const registerConditionalModules = conditionalModules.map((module) =>
@@ -17,6 +19,9 @@ const registerConditionalModules = conditionalModules.map((module) =>
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
